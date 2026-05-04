d82267f1f66f51e032334efb09e6a420d4503826
uvtechglobal <48288870+uvtechglobal@users.noreply.github.com>
2026-05-04 18:03:48 +0530

    Add professional UVTech workspace and post-login routing

---
+++ uvtech_biz/hooks.py
@@ -0,0 +1,29 @@
+app_name = "uvtech_biz"
+app_title = "UVTech Biz"
+app_publisher = "UVTech"
+app_description = "Multi-marketplace order and inventory SaaS"
+app_email = "support@uvtecherp.in"
+app_license = "MIT"
+
+scheduler_events = {
+    "cron": {
+        "*/15 * * * *": [
+            "uvtech_biz.scheduler.sync_jobs.sync_orders"
+        ]
+    }
+}
+
+fixtures = []
+
+
+after_migrate = [
+    "uvtech_biz.setup.ensure_setup"
+]
+
+after_install = "uvtech_biz.setup.ensure_setup"
+
+
+before_migrate = "uvtech_biz.setup.ensure_setup"
+
+
+on_session_creation = "uvtech_biz.branding.set_default_workspace"
